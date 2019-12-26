c5408daa9b2f3d943c58e1c1c264fb4569676cd0
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2019-12-26 18:03:21 -0500

    beckham: add a few missing blobs
    Change-Id: I4ef52c8eacc0dda27fc333a8e701eb77ae428b2f
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>

--- recovery/root/sbin/prepdecrypt.sh
+++ recovery/root/sbin/prepdecrypt.sh
@@ -58,13 +58,16 @@ fi
 ###### NOTE: The below is no longer used but I'm keeping it here in case it is needed again at some point!
 mkdir -p /vendor/lib64/hw/
 
+cp /s/system/lib64/android.hardware.boot@1.0.so /sbin/
+cp /s/system/lib64/android.hardware.confirmationui@1.0.so /sbin/
 cp /s/system/lib64/android.hidl.base@1.0.so /sbin/
 cp /s/system/lib64/libicuuc.so /sbin/
-cp /s/system/lib64/libxml2.so /sbin/
+cp /s/system/lib64/libion.so /sbin/
 cp /s/system/lib64/libkeymaster4support.so /sbin/
 cp /s/system/lib64/libkeymaster_messages.so /sbin/
 cp /s/system/lib64/libkeymaster_portable.so /sbin/
 cp /s/system/lib64/libsoftkeymasterdevice.so /sbin/
+cp /s/system/lib64/libxml2.so /sbin/
 
 relink /v/bin/qseecomd
 
